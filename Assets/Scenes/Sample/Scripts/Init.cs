1632c1dbef41505c5f5b13b23fe00c21ddff881c
Mahdioshani <mahdioshani2003@gmail.com>
2025-05-23 19:23:52 +0330

    Fix nuget dependancies.

---
+++ Assets/Scenes/Sample/Scripts/Init.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using odysseyAnalytics.Adapters.Logger;
+using odysseyAnalytics.Adapters.RabbitMQ;
+using odysseyAnalytics.Adapters.REST;
+using odysseyAnalytics.Adapters.Sqlite;
+using UnityEngine;
+using odysseyAnalytics.Core.Application.Session;
+using odysseyAnalytics.Core.Ports;
+using ILogger = odysseyAnalytics.Core.Ports.ILogger;
+
+public class Init : MonoBehaviour
+{
+    public SessionHandler session;
+    async void Start()
+    {
+        string token = "QSRBAtavWWSxfkbRKUtiXlZF1CoPQyYDDYaU5Bls5OrfgzXPTu9CjXVRFyjqlhSK";
+        RESTAdapter gatewayPort = new RESTAdapter("https://odysseyanalytics.ir/api/");
+        ILogger logger = new DefaultLogger();
+        RabbitMQAdapter rabbitMqAdapter = new RabbitMQAdapter();
+        SqliteAdapter sqliteAdapter = new SqliteAdapter(Path.Combine(Application.persistentDataPath,"events.db"));
+        session = new SessionHandler(gatewayPort,rabbitMqAdapter,rabbitMqAdapter,logger,sqliteAdapter,token);
+        await session.InitializeSessionAsync();
+        await session.StartSessionAsync("Android");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
